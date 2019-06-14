f0e17a6a6d3003d10e5d0f024fb95caffaec9942
Choong Hong Cheng <me@chclab.net>
2019-06-14 20:04:18 +0800

    Initial mocked login page commit

--- app/src/main/java/com/needknowers/community/MainActivity.java
+++ app/src/main/java/com/needknowers/community/MainActivity.java
@@ -15,18 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
