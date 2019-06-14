9c297dd4ae23e5096ff298137b36d63c13a71b5f
Choong Hong Cheng <me@chclab.net>
2019-06-14 19:48:07 +0800

    Change activity to fragment and added MainActivity

--- app/src/main/java/com/needknowers/community/MainActivity.java
+++ app/src/main/java/com/needknowers/community/MainActivity.java
@@ -10,12 +10,12 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class DirectionsActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_directions);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
