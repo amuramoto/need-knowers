e064c14fb5b6335835fd090c9f52b10452e6f9e4
liewjuntung <liewjuntung@gmail.com>
2019-06-15 15:02:28 +0800

    Added Removed java MainActivity

--- app/src/main/java/com/needknowers/community/WhereAmIFragment.kt
+++ app/src/main/java/com/needknowers/community/WhereAmIFragment.kt
@@ -14,23 +14,44 @@ import com.google.android.gms.maps.StreetViewPanoramaView
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.fragment_where_am_i.view.*
 import com.google.android.gms.maps.model.StreetViewPanoramaCamera
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import java.util.*
+import java.util.Arrays.asList
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.gms.tasks.Task
+
 
 
 
 
 class WhereAmIFragment : Fragment(), OnStreetViewPanoramaReadyCallback {
 
+    private lateinit var places: PlacesClient
     private var streetViewPanoramaView: StreetViewPanoramaView? = null
     lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     var longitude: Double? = null
     var latitude: Double? = null
     val TAG = NeedKnowerMainFragment::class.java.simpleName
 
+    // Use fields to define the data types to return.
+    var placeFields = listOf(Place.Field.NAME, Place.Field.ADDRESS)
+
+    // Use the builder to create a FindCurrentPlaceRequest.
+    var request = FindCurrentPlaceRequest.builder(placeFields).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        places = Places.createClient(context!!)
         requestLastLocation()
+        val placeResponse = places.findCurrentPlace(request)
+        placeResponse.addOnCompleteListener {
+            Log.d(TAG, it.toString())
+        }
 
     }
 
@@ -40,9 +61,10 @@ class WhereAmIFragment : Fragment(), OnStreetViewPanoramaReadyCallback {
                 Log.d(TAG, it.result.toString())
                 longitude = it.result?.longitude
                 latitude = it.result?.latitude
+                streetViewPanoramaView?.getStreetViewPanoramaAsync(this)
             }
         }
-        streetViewPanoramaView?.getStreetViewPanoramaAsync(this)
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -50,9 +72,9 @@ class WhereAmIFragment : Fragment(), OnStreetViewPanoramaReadyCallback {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_where_am_i, container, false)
         streetViewPanoramaView = view.findViewById(R.id.streetviewpanorama)
-
         streetViewPanoramaView?.onCreate(savedInstanceState)
         streetViewPanoramaView?.getStreetViewPanoramaAsync(this)
+
         return view
     }
 
