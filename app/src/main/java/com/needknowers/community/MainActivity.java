f5fcac15033a3d829971936c4c7e845100f9358e
Dixing Xu <dixingxu@gmail.com>
2019-06-15 14:37:57 +0800

    update MainActivity

--- app/src/main/java/com/needknowers/community/MainActivity.java
+++ app/src/main/java/com/needknowers/community/MainActivity.java
@@ -1,4 +1,14 @@
 package com.needknowers.community;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
